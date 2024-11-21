5f97c865fe1760961051b2909f195c95b742bfde
yachilT <128305238+yachilT@users.noreply.github.com>
2024-11-21 16:08:03 +0200

    finished part 3

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 
+unsigned char * halftone(unsigned char * buffer, int width, int height);
 unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int length, float gw, float rw, float bw);
 int main(void)
 {
@@ -15,7 +16,8 @@ int main(void)
     unsigned char newBuffer[width * height];
 
     grayscale(buffer, newBuffer, width * height, 0.2989, 0.5870, 0.1140);
-    int result = stbi_write_png("res/textures/new_Lenna.png", width, height, 1, newBuffer, width);
+    unsigned char * resBuff = halftone(newBuffer, width, height);
+    int result = stbi_write_png("res/textures/new_Lenna.png", width * 2, height * 2, 1, resBuff, width * 2);
     std::cout << result << std::endl;
     return 0;
 }
@@ -25,4 +27,40 @@ unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int
         newBuffer[i] = buffer[i * 4] * rw + buffer[i * 4 + 1] *gw + buffer[i * 4 + 2] * bw;
     }
     return newBuffer;
+}
+
+unsigned char * halftone(unsigned char * buffer, int width, int height) {
+    int length = width * height;
+    unsigned char * result = new unsigned char[length * 4];
+    for (int i = 0; i < length; i++) {
+        int row = i / width;
+        int col = i % width;
+        if (buffer[i] < 255.0 / 5) {
+            result[row * 2 * 2 * width + col * 2] = 0;
+            result[row * 2 * 2 * width + col * 2 + 1] = 0;
+            result[(row * 2 + 1) * 2 * width + col * 2] = 0;
+            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = 0;
+        } else if (buffer[i] >= 255.0 / 5 && buffer[i] < 255.0 / 5 * 2 ) {
+            result[row * 2 * 2 * width + col * 2] = 0;
+            result[row * 2 * 2 * width + col * 2 + 1] = 0;
+            result[(row * 2 + 1) * 2 * width + col * 2] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = 0;
+        } else if (buffer[i] >= 255.0 / 5 * 2 && buffer[i] < 255.0 / 5 * 3) {
+            result[row * 2 * 2 * width + col * 2] = 255;
+            result[row * 2 * 2 * width + col * 2 + 1] = 0;
+            result[(row * 2 + 1) * 2 * width + col * 2] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = 0;
+        } else if (buffer[i] >= 255.0 / 5 * 3 && buffer[i] < 255.0 / 5 * 4) {
+            result[row * 2 * 2 * width + col * 2] = 0;
+            result[row * 2 * 2 * width + col * 2 + 1] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = 255;
+        } else if (buffer[i] >= 255.0 / 5 * 4) {
+            result[row * 2 * 2 * width + col * 2] = 255;
+            result[row * 2 * 2 * width + col * 2 + 1] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2] = 255;
+            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = 255;
+        }
+    }
+    return result;
 }
