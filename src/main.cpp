2023547d97d1509d31f0e203a9a824b0dd236600
yachilT <128305238+yachilT@users.noreply.github.com>
2024-11-21 15:06:25 +0200

    finished part1 !

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,13 @@ int main(void)
     unsigned char * buffer = stbi_load(filepath.c_str(), &width, &height, &comps, req_comps);
     unsigned char newBuffer[width * height];
 
-    grayscale(buffer, newBuffer, width * height, 1/3, 1/3, 1/3);
-
+    grayscale(buffer, newBuffer, width * height, 0.2989, 0.5870, 0.1140);
     int result = stbi_write_png("res/textures/new_Lenna.png", width, height, 1, newBuffer, width);
     std::cout << result << std::endl;
     return 0;
 }
 
-unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int length, float gw, float rw, float bw) {
+unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int length, float rw, float gw, float bw) {
     for (int i = 0; i < length; i++) {
         newBuffer[i] = buffer[i * 4] * rw + buffer[i * 4 + 1] *gw + buffer[i * 4 + 2] * bw;
     }
