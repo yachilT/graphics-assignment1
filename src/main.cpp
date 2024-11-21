e75fc821f6e45ea390c8faae08b7e303953276b1
yachilT <128305238+yachilT@users.noreply.github.com>
2024-11-21 14:50:55 +0200

    part 1 (failed :( )

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,25 @@
 
 #include <iostream>
 
+unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int length, float gw, float rw, float bw);
 int main(void)
 {
-    std::string filepath = "res/textures/plane.png";
+    std::string filepath = "res/textures/Lenna.png";
     int width, height, comps;
     int req_comps = 4;
     unsigned char * buffer = stbi_load(filepath.c_str(), &width, &height, &comps, req_comps);
-    int result = stbi_write_png("res/textures/new_plane.png", width, height, req_comps, buffer, width * comps);
+    unsigned char newBuffer[width * height];
+
+    grayscale(buffer, newBuffer, width * height, 1/3, 1/3, 1/3);
+
+    int result = stbi_write_png("res/textures/new_Lenna.png", width, height, 1, newBuffer, width);
     std::cout << result << std::endl;
     return 0;
+}
+
+unsigned char * grayscale(unsigned char * buffer, unsigned char * newBuffer, int length, float gw, float rw, float bw) {
+    for (int i = 0; i < length; i++) {
+        newBuffer[i] = buffer[i * 4] * rw + buffer[i * 4 + 1] *gw + buffer[i * 4 + 2] * bw;
+    }
+    return newBuffer;
 }
