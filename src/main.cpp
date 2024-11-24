912ab68940572342fc62d29ab356cda4e3a7ffe8
FoyerD <amitfoye@post.bgu.ac.il>
2024-11-24 13:28:58 +0200

    outline part of canny working better

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@
 
 unsigned char * convolution(unsigned char * buffer, int width, int height, float * kernel, int kwidth, int kheight, float norm);
 unsigned char * grayscale(unsigned char * buffer, int length, float gw, float rw, float bw);
-void applyKernel(unsigned char * buffer,unsigned char * newBuffer, int width, int height, int x, int y, float * kernel, int kwidth, int kheight, float norm);
+void applyKernel(unsigned char * buffer, unsigned char * newBuffer, int width, int x, int y, float * kernel, int kwidth, int w, int h, float norm);
 unsigned char * canny(unsigned char * buffer, int width, int height, float scale);
 unsigned char * halftone(unsigned char * buffer, int width, int height);
-float capPixel(float p);
+float clipPixel(float p);
 
 int main(void)
 {
@@ -23,7 +23,7 @@ int main(void)
     unsigned char *greyBuffer = grayscale(buffer, width * height, 0.2989, 0.5870, 0.1140);
     int result = stbi_write_png("res/textures/grey_Lenna.png", width, height, 1, greyBuffer, width);
 
-    unsigned char *cannyBuffer = canny(greyBuffer, width, height, 5);
+    unsigned char *cannyBuffer = canny(greyBuffer, width, height, 2);
     result = result + stbi_write_png("res/textures/canny_Lenna.png", width, height, 1, cannyBuffer, width);
     std::cout << result << std::endl;
     return 0;
@@ -41,14 +41,21 @@ unsigned char * canny(unsigned char* buffer, int width, int height, float scale)
     float xSobel[] = {1,0,-1, 2,0,-2, 1,0,-1};
     float ySobel[] = {1,2,1, 0,0,0, -1,-2,-1};
 
-    unsigned char* xConv = convolution(buffer, width, height, xSobel, 3, 3, 9);
-    unsigned char* yConv = convolution(buffer, width, height, ySobel, 3, 3, 9);
+    unsigned char* xConv = convolution(buffer, width, height, xSobel, 3, 3, 4.0/scale);
+    unsigned char* yConv = convolution(buffer, width, height, ySobel, 3, 3, 4.0/scale);
     unsigned char* imageGradients = new unsigned char[width * height];
     float* imageAngels = new float[width * height];
 
     for(int i = 1; i < height - 1; i++){
         for(int j = 1; j < width - 1; j++){
-            imageGradients[i * width + j] = scale * std::sqrt(std::pow((float)(int)xConv[i * width + j], 2) + std::pow((float)(int)yConv[i * width + j], 2));
+            imageGradients[i * width + j] = std::sqrt(std::pow((float)(int)xConv[i * width + j], 2) + std::pow((float)(int)yConv[i * width + j], 2));
+            imageAngels[j + i * width] = std::atan2(xConv[j + i * width], yConv[j + i * width]);
+        }
+    }
+
+    for(int i = 1; i < height - 1; i++){
+        for(int j = 1; j < width - 1; j++){
+            
         }
     }
 
@@ -61,25 +68,24 @@ unsigned char * convolution(unsigned char * buffer, int width, int height, float
     int w = (kwidth - 1)/2;
     for(int i = h; i < height - h; i++){
         for(int j = w; j < width - w; j++){
-            applyKernel(buffer, newBuffer,width, height, j, i, kernel, kwidth, kheight, norm);
+            applyKernel(buffer, newBuffer,width, j, i, kernel, kwidth, w, h, norm);
         }
     }
 
     return newBuffer;
 }
 
-void applyKernel(unsigned char * buffer, unsigned char * newBuffer, int width, int height, int x, int y, float * kernel, int kwidth, int kheight, float norm){
+void applyKernel(unsigned char * buffer, unsigned char * newBuffer, int width, int x, int y, float * kernel, int kwidth, int w, int h, float norm){
     float sum = 0;
-    for(int i = 0; i < kheight; i++){
-        for(int j = 0; j < kwidth; j++){
-            sum += (buffer[(x + j - kwidth/2) + (y + i - kheight) * width]) * kernel[j + i * kwidth];
+    for(int i = y-h; i <= y+h; i++){
+        for(int j = x-w; j <= x+w; j++){
+            sum += (buffer[j + (i) * width]) * kernel[j+w-x + (i+h-y) * kwidth];
         }
     }
-    sum = sum / norm; 
-    newBuffer[x + y * width] = capPixel(sum);
+    newBuffer[x + y * width] = clipPixel(sum / norm);
 }
 
-float capPixel(float p){
+float clipPixel(float p){
     p = p > 255 ? 255 : p;
     p = p < 0 ? 0 : p;  
     return p;
